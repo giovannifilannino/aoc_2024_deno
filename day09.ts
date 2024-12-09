fe2c9b18338ab04b2b32ff7a0c8491c214525f23
Giovanni Filannino <giovanni@3ap.ch>
2024-12-09 22:36:30 +0100

    day9 complete

--- day09.ts
+++ day09.ts
@@ -24,24 +24,58 @@ function part1(content: string[]): number {
   return getChecksum(result);
 }
 
-function part2(): number {
-  return 0;
+function part2(content: string[][]): number {
+  let defragmentingDisk: string[][] = [...content];
+
+  let cursor = defragmentingDisk.length - 1;
+
+  while (cursor >= 0) {
+    if (defragmentingDisk[cursor].includes(".")) {
+      cursor--;
+      continue;
+    }
+
+    const emptyIndex = defragmentingDisk.findIndex(
+      (x, i) => i < cursor && x.includes(".") && x.length >= defragmentingDisk[cursor].length,
+    );
+
+    if (emptyIndex === -1) {
+      cursor--;
+      continue;
+    }
+
+    const diff = defragmentingDisk[emptyIndex].length - defragmentingDisk[cursor].length;
+
+    defragmentingDisk[emptyIndex] = [...defragmentingDisk[cursor]];
+    defragmentingDisk[cursor] = Array(defragmentingDisk[cursor].length).fill(".");
+
+    if (diff !== 0) {
+      defragmentingDisk = [
+        ...defragmentingDisk.slice(0, emptyIndex + 1),
+        Array(diff).fill("."),
+        ...defragmentingDisk.slice(emptyIndex + 1),
+      ];
+      cursor++;
+    }
+
+    cursor--;}
+
+  return getChecksum(defragmentingDisk.flat());
 }
 
-function convertStringToMapMemory(content: string): string[] {
+function convertStringToMapMemory(content: string): string[][]{
   const chunkyDisk: string[][] = content 
   .split("")
   .map((x, i) => Array(parseInt(x)).fill(i % 2 === 0 ? Math.floor(i / 2) : "."));
-
-  return chunkyDisk.flat();
+  return chunkyDisk;
 }
 
 export async function showResultsDay09() {
   const file = await Deno.readTextFile("inputs/day09.txt");
   const content = convertStringToMapMemory(file);
 
-  const resultPart1 = part1(content);
-  const resultPart2 = part2();
+  const resultPart1 = part1(content.flat());
+  const resultPart2 = part2(content);
   console.log("Day 09");
   console.log("--------");
   console.log(`Result part1: ${resultPart1}`);
